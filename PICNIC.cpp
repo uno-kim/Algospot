70fe572dfcf8bc94a232862099abaaf06c13cb54
Min Sung Kim <ms.uno.kim@gmail.com>
2017-01-04 01:01:12 +0900

    문제: https://algospot.com/judge/problem/read/PICNIC 답안: https://algospot.com/judge/submission/detail/486936

---
+++ PICNIC.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <algorithm>
+#include <string.h>
+using namespace std;
+int T, N, M, u, v, ans;
+bool areFriends[10][10];
+// taken[i] : i 번째 학생이 짝을 찾았으면 true
+int countParings(bool taken[10])
+{
+    int from = -1;
+    for (int i = 0; i < N; ++i)
+        if (!taken[i])
+        {
+            from = i;
+            break;
+        }
+    if (from == -1) return 1; // 짝을 모두 찾은 경우
+    int ret = 0;
+    for (int to = from + 1; to < N; ++to)
+        if (!taken[to] && areFriends[from][to])
+        {
+            taken[from] = taken[to] = true;
+            ret += countParings(taken);
+            taken[from] = taken[to] = false;
+        }
+    return ret;
+}
+
+int main()
+{
+    //freopen("PICNIC.txt", "rt", stdin);
+    scanf("%d", &T);
+    while (T--)
+    {
+        scanf("%d%d", &N, &M);
+        memset(areFriends, false, sizeof(areFriends));
+        for (int i = 0; i < M; ++i)
+        {
+            scanf("%d%d", &u, &v);
+            areFriends[u][v] = areFriends[v][u] = true;
+        }
+        bool selected[10] = { false, };
+        printf("%d\n", countParings(selected));
+    }
+    return 0;
+}
