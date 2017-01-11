65bd5e686c2a6ca0698db9e73bee1ac61415fedd
Min Sung Kim <ms.uno.kim@gmail.com>
2017-01-12 00:20:28 +0900

    JUMPGAME 문제: https://algospot.com/judge/problem/read/JUMPGAME 답안: https://algospot.com/judge/submission/detail/488918

--- JUMPGAME.cpp
+++ JUMPGAME.cpp
@@ -6,12 +6,11 @@ int cache[102][102];
 // (y, x) 에서 마지막까지 도달 가능한지 여부
 int jump(int y, int x)
 {
-    if (y == N - 1 && x == N - 1) return 1;
     if (x >= N || y >= N) return 0;
     int &ret = cache[y][x];
     if (ret != -1) return ret;
     ret = 0;
-    ret = (jump(y + in[y][x], x) | jump(y, x + in[y][x]));
+    ret = (jump(y + in[y][x], x) || jump(y, x + in[y][x]));
     return ret;
 }
 
@@ -26,6 +25,7 @@ int main()
             for (int x = 0; x < N; ++x)
                 scanf("%d", &in[y][x]);
         memset(cache, -1, sizeof(cache));
+        cache[N - 1][N - 1] = 1;
         if (jump(0, 0)) printf("YES\n");
         else printf("NO\n");
     }
