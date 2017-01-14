97b6f24a9a02d1bc90c73b0c91986c427d368892
Min Sung Kim <ms.uno.kim@gmail.com>
2017-01-14 22:23:58 +0900

    LIS 문제: https://algospot.com/judge/problem/read/LIS 답안: https://algospot.com/judge/submission/detail/489692

---
+++ LIS_solution1.cpp
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <algorithm>
+using namespace std;
+int T, N, S[502], cache[502];
+// S[start] 에서 시작하는 증가 부분 수열 중 최대 길이
+int lis(int start)
+{
+    int& ret = cache[start];
+    if (ret != -1) return ret;
+    ret = 1;
+    for (int next = start + 1; next <= N; ++next)
+        if(S[start] < S[next])
+            ret = max(ret, lis(next) + 1);
+    return ret;
+}
+
+int main()
+{
+    //freopen("LIS.txt", "rt", stdin);
+    scanf("%d", &T);
+    while (T--)
+    {
+        scanf("%d", &N);
+        for (int i = 1; i <= N; ++i)
+            scanf("%d", &S[i]);
+        memset(cache, -1, sizeof(cache));
+        printf("%d\n", lis(0) - 1);
+    }
+    return 0;
+}
---
+++ LIS_solution2.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+using namespace std;
+int T, N, in, C[502], lenC; // C[i] = 지금까지 만든 부분 배열이 갖는 길이 i인 증가 부분 수열 중 최소의 마지막 값
+int main()
+{
+    //freopen("LIS.txt", "rt", stdin);
+    scanf("%d", &T);
+    while (T--)
+    {
+        scanf("%d", &N);
+        scanf("%d", &C[0]); // C[0] 에 처음 입력값을 바로 넣는다.
+        lenC = 1;
+        for (int i = 1; i < N; ++i)
+        {
+            scanf("%d", &in);
+            for (int j = 0; j < lenC; ++j)
+                if (C[j] >= in)
+                {
+                    C[j] = in;
+                    break;
+                }
+            if (C[lenC - 1] < in)
+                C[lenC++] = in;
+        }
+        printf("%d\n", lenC);
+    }
+    return 0;
+}
