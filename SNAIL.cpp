86659d3082a2d13b177920434c03bb82ba389050
Min Sung Kim <ms.uno.kim@gmail.com>
2017-01-20 00:45:22 +0900

    SNAIL 문제: https://algospot.com/judge/problem/read/SNAIL 답안: https://algospot.com/judge/submission/detail/491018

---
+++ SNAIL.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <string.h>
+int T, N, M;
+double cache[1002][2004];
+int main()
+{
+    //freopen("SNAIL.txt", "rt", stdin);
+    scanf("%d", &T);
+    cache[1][1] = 0.25;
+    cache[1][2] = 0.75;
+    for (int d = 2; d <= 1000; ++d)
+        for (int h = 2; h <= d * 2; ++h)
+            cache[d][h] = cache[d - 1][h - 2] * 0.75 + cache[d - 1][h - 1] * 0.25;
+    while (T--)
+    {
+        scanf("%d%d", &N, &M);
+        double ret = 0.0;
+        for (int n = N; n <= 2 * M; ++n)
+            ret += cache[M][n];
+        printf("%0.10lf\n", ret);
+    }
+    return 0;
+}
