feb23c95fd4c93b2773e94aab3a9f75af5490c5d
Min Sung Kim <ms.uno.kim@gmail.com>
2017-01-19 22:41:56 +0900

    TILING2 문제: https://algospot.com/judge/problem/read/TILING2 답안: https://algospot.com/judge/submission/detail/490993

---
+++ TILING2.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <string.h>
+#define MOD 1000000007
+int T, N, cache[102];
+int main()
+{
+    cache[1] = 1, cache[2] = 2;
+    for (int i = 3; i <= 100; ++i)
+        cache[i] = (cache[i - 1] + cache[i - 2]) % MOD;
+    scanf("%d", &T);
+    while (T--)
+    {
+        scanf("%d", &N);
+        printf("%d\n", cache[N]);
+    }
+    return 0;
+}
