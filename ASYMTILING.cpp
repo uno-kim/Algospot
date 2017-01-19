09df86c9bb85d8ae68ba924d4ff35cbe4c704cd5
Min Sung Kim <ms.uno.kim@gmail.com>
2017-01-20 01:23:35 +0900

    ASYMTILING 문제: https://algospot.com/judge/problem/read/ASYMTILING 답안: https://algospot.com/judge/submission/detail/491026

---
+++ ASYMTILING.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <string.h>
+#define MOD 1000000007
+int T, N;
+int tile[102]; // 2 * n 크기의 모든 타일링 개수
+int ans[102]; // 2 * n 크기의 모든 타일링 개수 - 대칭 타일링 개수
+int main()
+{
+    tile[1] = 1, tile[2] = 2;
+    for (int i = 3; i <= 100; ++i)
+        tile[i] = (tile[i - 1] + tile[i - 2]) % MOD;
+    for (int i = 3; i <= 99; i += 2) // 짝수
+        ans[i] = (tile[i] + MOD - tile[i / 2]) % MOD;
+    for (int i = 4; i <= 100; i += 2) // 홀수
+    {
+        ans[i] = (tile[i] + MOD - tile[i / 2]) % MOD;
+        ans[i] = (ans[i] + MOD - tile[(i - 2) / 2]) % MOD;
+    }
+    scanf("%d", &T);
+    while (T--)
+    {
+        scanf("%d", &N);
+        printf("%d\n", ans[N]);
+    }
+    return 0;
+}
