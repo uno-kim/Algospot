5e5e84d1c67affe67dd1cc2e809cc1911f6357bd
Min Sung Kim <ms.uno.kim@gmail.com>
2017-01-22 23:51:36 +0900

    POLY 문제: https://algospot.com/judge/problem/read/POLY 답안: https://algospot.com/judge/submission/detail/491746

---
+++ POLY.cpp
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+#include <string.h>
+using namespace std;
+const int MOD = 10000000;
+int T, N, cache[102][102];
+// 남은 n 개 블록 중 현재 층을 r 개로 구성
+int poly(int n, int r)
+{
+    int& ret = cache[n][r];
+    if (ret != -1) return ret;
+    ret = 0;
+    for (int i = 1; i <= n - r; ++i)
+        ret = (ret + (r + i - 1) * poly(n - r, i) % MOD) % MOD;
+    return ret;
+}
+
+int main()
+{
+    //freopen("POLY.txt", "rt", stdin);
+    memset(cache, -1, sizeof(cache));
+    for (int i = 0; i <= 100; ++i) cache[i][i] = 1;
+    scanf("%d", &T);
+    while (T--)
+    {
+        scanf("%d", &N);
+        int ret = 0;
+        for (int i = 1; i <= N; ++i)
+            ret = (ret + poly(N, i)) % MOD;
+        printf("%d\n", ret);
+    }
+    return 0;
+}
